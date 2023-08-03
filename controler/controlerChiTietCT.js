0c14aea4da3c9ed4167859aa852c126555909753
hung90909 <114125660+hung90909@users.noreply.github.com>
2023-08-03 16:02:57 +0700

    updateAssi

--- controler/controlerChiTietCT.js
+++ controler/controlerChiTietCT.js
@@ -10,6 +10,7 @@ app.use(express.json())
 
 app.post("/addGiaoDich" ,async (req, res) => {
    try {
+    console.log(req.body)
      const chiTieu = new chiTietCT(req.body);
      await chiTieu.save();
      res.send("luu thanh cong")
